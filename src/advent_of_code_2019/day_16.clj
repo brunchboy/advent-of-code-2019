8b22a939c1fe497511e0f240513808c491d12e82
James Elliott <>
2019-12-17 16:09:14 -0600

    Part 2 for day 16, my crappy version.

--- src/advent_of_code_2019/day_16.clj
+++ src/advent_of_code_2019/day_16.clj
@@ -22,6 +22,7 @@
     (map #(mod (Math/abs %) 10) sums)))
 
 (defn fft
+  "Solves part 1."
   [input phases]
   (loop [digits (split-digits input)
          phases phases]
@@ -31,20 +32,42 @@
 
 ;; Part 2
 
-(defn fft-element-phase
-  "Returns a single element of the fft algorithm for an input value."
-  [digits element]
-  (let [sum (apply + (map * digits (build-fft-pattern element)))]
-    (mod (Math/abs sum) 10)))
+(defn fft2-phase
+  "A slightly more efficient version of fft, based on noticing some
+  patterns about which digits matter, but I hate this, it still took
+  about two hours to find the solution."
+  [digits]
+  (let [sums (loop [result    []
+                    sum       0
+                    remaining digits]
+               (if-let [current (first remaining)]
+                 (recur (conj result sum)
+                        (+ sum current)
+                        (rest remaining))
+                 result))]
+    (loop [result []
+           i      0]
+      (if (< i (count digits))
+        (recur
+         (conj result (loop [element 0
+                             sign    1
+                             offsets (range i (count digits) (* 2 (inc i)))]
+                        (if-let [offset (first offsets)]
+                          (let [element (+ element (* sign (- (nth sums (min (+ i offset 1) (dec (count sums))))
+                                                              (nth sums offset))))]
+                     (recur element (- sign) (rest offsets)))
+                          (mod (Math/abs element) 10))))
+         (inc i))
+        result))))
 
-(defn fft-element
-  "Calculate a single result element of the fft for a given number of phases."
-  [input element phases]
-  (loop [digits (split-digits input)
+(defn fft2
+  "Solve part 2... oh so slowly."
+  [input phases]
+  (loop [digits (vec (apply concat (repeat 10000 (split-digits input))))
          phases phases]
     (if (pos? phases)
-      (recur (fft-element-phase digits element) (dec phases))
-      ;; No... this doesn't pan out, we still need to calulate all elements for input to the next phase.
-      ;; I am sure there is some clever trick involving the fact we are doing things modulo 10, and/or
-      ;; that the patterns are made up of just 0, 1, and -1... but this is outside my linear algebra reach.
-      )))
+      (do
+        (println "phases:" phases)
+        (recur (fft2-phase digits) (dec phases)))
+      (let [offset (Long/valueOf (subs input 0 7))]
+        (clojure.string/join (take 8 (drop offset digits)))))))
