d46f0e84d24c7cd39152fcf94ba0905738af1134
James Elliott <james@deepsymmetry.org>
2019-12-18 23:58:49 -0600

    Day 19 solutions.

---
+++ test/advent_of_code_2019/day_19_test.clj
@@ -0,0 +1,16 @@
+(ns advent-of-code-2019.day-19-test
+  (:require [advent-of-code-2019.day-19 :as sut]
+            [clojure.test :as t]
+            [clojure.test :as test]))
+
+(test/deftest origin-covered
+  (test/is (sut/beam-covers? 0 0)))
+
+(test/deftest out-x-axis-not-covered
+  (test/is (not (sut/beam-covers? 100 0))))
+
+(test/deftest part-1
+  (test/is (= 158 (sut/part-1))))
+
+(test/deftest part-2
+  (test/is (= [6191165] (sut/part-2))))
