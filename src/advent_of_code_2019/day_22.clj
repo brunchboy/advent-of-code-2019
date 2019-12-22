8ed68d19f6f2bb7d17568d014ed69f778e33bf43
James Elliott <james@deepsymmetry.org>
2019-12-22 17:45:59 -0600

    Solve day 22 part 2. So very much math!

--- src/advent_of_code_2019/day_22.clj
+++ src/advent_of_code_2019/day_22.clj
@@ -3,12 +3,19 @@
   (:require [clojure.math.combinatorics :as combo]
             [clojure.repl :refer :all]))
 
+(defn cut-location
+  "Determines the deck position at which a cut should be performed,
+  transforming negative numbers into counting from the bottom of the
+  deck."
+  [deck-size where]
+  (if (pos? where)
+    where
+    (+ deck-size where)))
+
 (defn cut
   [deck where]
   (println "cut" where)
-  (if (pos? where)
-    (vec (apply concat (reverse (split-at where deck))))
-    (vec (apply concat (reverse (split-at (+ (count deck) where) deck))))))
+  (vec (apply concat (reverse (split-at (cut-location (count deck) where) deck)))))
 
 (defn deal-with-increment
   [deck increment]
@@ -43,7 +50,7 @@
   [deck instructions]
   (reduce shuffle-step deck (clojure.string/split-lines instructions)))
 
-(def part-1-instructions
+(def shuffling-instructions
   "deal into new stack
 deal with increment 32
 cut 5214
@@ -150,4 +157,106 @@ deal into new stack")
 
 (defn part-1
   []
-  (.indexOf (shuffle-deck factory-deck part-1-instructions) 2019))
+  (.indexOf (shuffle-deck factory-deck shuffling-instructions) 2019))
+
+;; Part 2
+
+(defn card-after-cut
+  "Returns the position from which a card was taken to reach a specified
+  position in the deck after the deck has been cut at the specified
+  location. As with part 1, negative locations mean to count from the
+  bottom."
+  [deck-size cut-spec ending-position]
+  (mod (+ cut-spec ending-position) deck-size))
+
+(defn card-after-reverse
+  "Returns the position from which a card was taken to reach a specified
+  position in the deck after the deck has been reversed."
+  [deck-size ending-position]
+  (- deck-size ending-position 1))
+
+;; This turns out only to work when the increment is smaller than about half
+;; of the deck size. Time to break out more number theory...
+(defn card-after-deal-with-increment
+  "Returns the position from which a card was taken to reach a specified
+  position in the deck after the deck has been shuffled by dealing
+  with the specified increment."
+  [deck-size increment ending-position]
+  (let [cards-per-pass   (inc (quot deck-size increment))
+        pass-number      (mod ending-position increment)
+        card-within-pass (quot ending-position increment)]
+    (+ (* pass-number cards-per-pass) card-within-pass)))
+
+;; Part 2 take two
+
+(def deck-size
+  "The number of cards in the crazy big deck."
+  (biginteger 119315717514047))
+
+(def shuffle-count
+  "The number of times we need to run our shuffle algorithm."
+  (biginteger 101741582076661))
+
+;; To simplify the math greatly, the result of any combination of our
+;; shuffle steps can be described as a combination of the number of
+;; cards in the deck, an offset (describing where the deck was cut,
+;; with the factory deck being offset 0), and an increment (where the
+;; factory deck has an increment of 1, so each card is one higher than
+;; the preceding card).
+
+(defn nth-card
+  "This function returns the nth card from a deck described by the tuple
+  of `num-cards`, `offset` and `increment`."
+  [[num-cards offset increment] i]
+  (.mod (.add offset (.multiply i increment)) num-cards))
+
+(defn shuffle-step-2
+  "Calculates the resulting offset and increment parameters for an input
+  deck and a shuffling instruction."
+  [[num-cards offset increment] instruction]
+  (or
+   (when (= instruction "deal into new stack") ; Reverse the deck:
+     (let [new-increment (.mod (.negate increment) num-cards)]  ; Negate the increment but keep it in legal range.
+       [num-cards (.mod (.add offset new-increment) num-cards) new-increment]))  ; Then rotate deck one card left.
+
+   (when-let [matches (re-matches #"cut (-?\d+)" instruction)] ; Rotate the deck a number of cards to the left.
+     (let [distance (biginteger (last matches))]
+       [num-cards (.mod (.add offset (.multiply distance increment)) num-cards) increment]))
+
+   (when-let [matches (re-matches #"deal with increment (\d+)" instruction)]  ; Compose increments.
+     ;; The increment is multipled by the modular inverse of the new increment.
+     (let [deal-increment (biginteger (last matches))]
+       [num-cards offset (.mod (.multiply increment (.modInverse deal-increment num-cards)) num-cards)]))
+
+   (println "Unrecognized instruction:" instruction)))
+
+(defn shuffle-deck-2
+  "Calculates the offset and increment parameters resulting from a set
+  of shuffling instructions on a factory deck."
+  [deck instructions]
+  (reduce shuffle-step-2 deck (clojure.string/split-lines instructions)))
+
+(defn factory-deck-2
+  "Creates a factory deck for part 2. With no arguments uses the full
+  problem deck size. For unit testing can supply a deck size."
+  ([]
+   (factory-deck-2 deck-size))
+  ([size]
+   [(biginteger size) (biginteger 0) (biginteger 1)]))
+
+(defn repeat-shuffles
+  "Calculates the results from repeating a particular shuffle multiple times."
+  [[num-cards offset increment] iterations]
+  (let [new-increment (.modPow increment iterations num-cards)
+        base (.mod (.subtract BigInteger/ONE increment) num-cards)]
+    [num-cards
+     (.mod (.multiply offset (.multiply (.subtract BigInteger/ONE new-increment)
+                                        (.modInverse base num-cards)))
+           num-cards)
+     new-increment]))
+
+(defn part-2
+  "Solve part 2"
+  []
+  (let [shuffled (shuffle-deck-2 (factory-deck-2) shuffling-instructions)]
+    (nth-card (repeat-shuffles shuffled shuffle-count) (biginteger 2020))))
--- test/advent_of_code_2019/day_22_test.clj
+++ test/advent_of_code_2019/day_22_test.clj
@@ -11,7 +11,7 @@
   (test/is (= [3 4 5 6 7 8 9 0 1 2] (sut/cut ten-deck 3)))
   (test/is (= [6 7 8 9 0 1 2 3 4 5] (sut/cut ten-deck -4))))
 
-(test/deftest cycle
+(test/deftest skip-deal
   (test/is (= [0 7 4 1 8 5 2 9 6 3] (sut/deal-with-increment ten-deck 3))))
 
 (test/deftest shuffling
@@ -39,3 +39,71 @@ cut 3
 deal with increment 9
 deal with increment 3
 cut -1"))))
+
+(test/deftest part-1
+  (test/is (= 6129 (sut/part-1))))
+
+;; Part 2
+
+(def prime-deck
+  "A still-small deck but with a prime number of cards for
+  experimenting with formulae for part 2."
+  (vec (range 11)))
+
+(test/deftest cut-formula
+  "Checks that my shortcut for calculating the results of cuts
+  works."
+  []
+  (let [deck-size (count prime-deck)]
+    (test/is (empty? (filter identity
+                             (for [cut-spec (range (- deck-size) deck-size)
+                                   pos      (range deck-size)]
+                               (not= (sut/card-after-cut deck-size cut-spec pos)
+                                     (nth (sut/cut prime-deck cut-spec) pos))))))))
+
+(test/deftest reverse-formula
+  "Checks that my shortcut for calculating the results of deck
+  reversals works."
+  []
+  (let [deck-size (count prime-deck)]
+    (test/is (empty? (filter identity
+                             (for [pos (range deck-size)]
+                               (not= (sut/card-after-reverse deck-size pos)
+                                     (nth (sut/deal-into-new-stack prime-deck) pos))))))))
+
+;; Abandoned for second approach.
+#_(test/deftest deal-with-increment-formula
+  "Checks that my shortcut for calculating the results of cuts
+  works."
+  []
+  (let [deck-size (count prime-deck)]
+    (test/is (empty? (vec (filter identity
+                                  (for [increment (range 1 deck-size)
+                                        pos       (range deck-size)]
+                                    (let [failed? (not= (sut/card-after-deal-with-increment deck-size increment pos)
+                                                        (nth (sut/deal-with-increment prime-deck increment) pos))]
+                                      (when failed?
+                                        (println)
+                                        (println "failed for increment" increment "and pos" pos))
+                                      failed?))))))))
+
+(test/deftest part-2-shuffles
+  (let [deck (sut/factory-deck-2 11)]
+    (let [shuffled (sut/shuffle-deck-2 deck "deal into new stack")]
+      (test/is (= [10 9 8 7 6 5 4 3 2 1 0]
+                  (map #(sut/nth-card shuffled (biginteger %)) (range 11)))))
+    (let [shuffled (sut/shuffle-deck-2 deck "cut 2")]
+      (test/is (= [2 3 4 5 6 7 8 9 10 0 1]
+                  (map #(sut/nth-card shuffled (biginteger %)) (range 11)))))
+    (let [shuffled (sut/shuffle-deck-2 deck "cut -3")]
+      (test/is (= [8 9 10 0 1 2 3 4 5 6 7]
+                  (map #(sut/nth-card shuffled (biginteger %)) (range 11)))))))
+
+;; And now for the moment of truth: try comparing results between part 1 and part 2.
+(test/deftest parts-equivalent
+  (test/is (= 3201
+              (sut/nth-card (sut/shuffle-deck-2 (sut/factory-deck-2 10007) sut/shuffling-instructions)
+                            (biginteger 2019)))))
+
+(test/deftest part-2
+  (test/is (= 71345377301237 (sut/part-2))))
